d8bb96f1d508e8766d6108b260bd20222156ad25
marlbones <mdimaanods@gmail.com>
2017-06-22 17:31:25 +1000

    Removed member do from nesting

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   resources :reports do
-    member do
       resources :pages do
-        member do
           resources :sections
-        end
       end
-    end
   end
   root 'reports#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
